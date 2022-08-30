eb0b4d16b6f281af3bf25a58adb514a4caf3101b
Junhao <junhao_code@163.com>
2022-08-30 18:25:11 +0800

    success commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author JunHao Yu
+ * @version 1.0
+ * @Description: <br/>
+ * @date 2022/08/30  18:21
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
