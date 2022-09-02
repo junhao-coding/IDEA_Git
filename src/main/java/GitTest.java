fe9709fb9cd66c5b1104033366ff24f65f418d5f
Junhao <junhao_code@163.com>
2022-09-02 22:19:20 +0800

    second commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("123");
         System.out.println("123");
+        System.out.println("yjh xue java");
     }
 }
