758f64ff852a4e28fb95c4a1871d3ede829ee815
NightsWatch <83063950+junhao-coding@users.noreply.github.com>
2022-09-02 22:47:31 +0800

    pull commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("master commit");
         System.out.println("hot-fix2");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
